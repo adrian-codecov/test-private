50920c96d3ddd34214b4aa01d687e041d1381d12
Adrian <adrian@codecov.io>
2024-09-26 15:13:47 -0600

    Add subtract test

--- src/math.test.js
+++ src/math.test.js
@@ -1,5 +1,9 @@
-const { add } = require("./math");
+const { add, subtract } = require("./math");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
 });
+
+test("subtracts 4 - 1 to equal 3", () => {
+    expect(subtract(4, 1)).toBe(3);
+  });
