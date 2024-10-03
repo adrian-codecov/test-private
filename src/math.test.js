413654d6aad23dbb150c8f79fc469afdf5edaf97
Adrian <adrian@codecov.io>
2024-10-03 09:14:57 -0700

    delete divide fn and test

--- src/math.test.js
+++ src/math.test.js
@@ -1,4 +1,4 @@
-const { add, subtract, multiply, divide, ajaa, bee, wow } = require("./math");
+const { add, subtract, multiply, ajaa, bee, wow } = require("./math");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
@@ -11,7 +11,3 @@ test("subtracts 4 - 1 to equal 3", () => {
 // test("multiply 3 and 3 to equal 9", () => {
 //   expect(multiply(3,3)).toBe(9);
 // });
-
-test("divide 6 and 2 to equal 3", () => {
-  expect(divide(6, 2)).toBe(3);
-});
