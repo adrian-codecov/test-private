a8c5f722df77a4927164aa8ce17991db77207740
Adrian <adrian@codecov.io>
2024-10-02 20:10:07 -0700

    delete test

--- src/math.test.js
+++ src/math.test.js
@@ -8,9 +8,9 @@ test("subtracts 4 - 1 to equal 3", () => {
   expect(subtract(4, 1)).toBe(3);
 });
 
-test("multiply 3 and 3 to equal 9", () => {
-  expect(multiply(3,3)).toBe(9);
-});
+// test("multiply 3 and 3 to equal 9", () => {
+//   expect(multiply(3,3)).toBe(9);
+// });
 
 test("divide 6 and 2 to equal 3", () => {
   expect(divide(6, 2)).toBe(3);
